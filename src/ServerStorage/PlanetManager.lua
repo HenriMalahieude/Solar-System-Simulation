f6d56036159a667d7c10021b7be26346bca5affb
HenriMalahieude <71425015+HenriMalahieude@users.noreply.github.com>
2020-09-16 23:32:18 -0700

    Remove unnecessary commented code

--- src/ServerStorage/PlanetManager.lua
+++ src/ServerStorage/PlanetManager.lua
@@ -16,33 +16,6 @@ local module = function(script)
 	
 	Planet.Anchored = true
 	
---	local aPos = Instance.new("AlignPosition")
---	aPos.ApplyAtCenterOfMass = true
---	aPos.Enabled = true
---	--aPos.RigidityEnabled = true
---	aPos.Responsiveness = 200
---	aPos.Parent = Planet
---	
---	local att0 = Instance.new("Attachment")
---	att0.Name = "Center"
---	att0.Parent = Planet
---	
---	local goto = Instance.new("Part")
---	goto.Name = "GoToPart"
---	goto.Anchored = true
---	goto.CanCollide = false
---	goto.Size = Vector3.new(1, 1, 1)
---	goto.Transparency = 1
---	goto.Position = Position
---	goto.Parent = workspace.MovementNodes
---	
---	local att1 = Instance.new("Attachment")
---	att1.Name = "GotoCenter"
---	att1.Parent = goto
---	
---	aPos.Attachment0 = att0
---	aPos.Attachment1 = att1
-	
 	--Wait until the Visualization script has stopped visualizing
 	repeat wait() until StartRendering.Value
 	
@@ -61,13 +34,7 @@ local module = function(script)
 		end
 		
 		Position = Position + Velocity*(1/60) --Match render rate with Visualizer program
-		
 		Planet.Orientation = Vector3.new(0, 0, 0)
-		
---		aPos.MaxVelocity = Velocity.Magnitude * 100000000000000000000
---		aPos.MaxForce = Velocity.Magnitude * 1000000000000000000000
---		
---		goto.Position = Position
 		Planet.Position = Position
 	end)
 end
